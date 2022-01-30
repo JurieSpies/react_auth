37e3861146d9f570ff96be423071bb8218afe6bb
Jurie_OV <jurie.spies@openvantage.co.za>
2022-01-30 09:43:13 +0200

    Router added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Reset from './pages/Reset/Reset';
+import Dashboard from './pages/Dashboard/Dashboard';
+import styled from "styled-components";
+
+const NavContainer = styled.nav`
+  display: flex;
+  flex-direction: column;
+`;
+
+
+const MainContainer = styled.div`
+  text-align: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+    <MainContainer >
+      <NavContainer >
+        <a href='/'>Home</a>
+        <a href='/login'>Login</a>
+        <a href='/reset'>Reset</a>
+        <a href='/dashboard'>Dashboard</a>
+      </NavContainer>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/reset" element={<Reset />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+    </MainContainer>
+    </Router>
   );
 }
 
